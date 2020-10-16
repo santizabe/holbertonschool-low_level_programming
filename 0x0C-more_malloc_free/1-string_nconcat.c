bc28e460c4afa17d2b57e2e50a18e4f528047f36
santizabe <santizabe@gmail.com>
2020-10-16 20:13:33 +0000

    Done

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -20,13 +20,21 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		;
 	for (s2count = 0; s2[s2count]; s2count++)
 		;
-	s2count > n ? (s2count = n) : (n = s2count);
+	if( s2count > n) 
+		s2count = n;  
+	else
+		n = s2count;
 	sizeBuffer = s1count + s2count + 1;
 	p = malloc(sizeBuffer * sizeof(char));
 	if (p == NULL)
 		return (NULL);
 	for (i = 0; i < sizeBuffer - 1; i++)
-		i < s1count ? (p[i] = s1[i]) : (p[i] = s2[i - s1count]);
-	p[sizeBuffer] = '\0';
+	{
+		if(i < s1count) 
+			p[i] = s1[i];
+		else
+			p[i] = s2[i - s1count];
+	}
+	p[sizeBuffer-1] = '\0';
 	return (p);
 }
