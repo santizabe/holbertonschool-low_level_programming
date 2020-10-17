07cc30b392e0800fe3b45b06b1de26df38b279ca
santizabe <santizabe@gmail.com>
2020-10-17 04:48:32 +0000

    Done

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,21 +1,25 @@
-#include <stdlib.h>
+#include "holberton.h"
+
 /**
  * _calloc - allocates memory for an initialized buffer
  * @nmemb: number of memory bytes
  * @size: number of data type bits
  * Return: return pointer to initialized buffer
  */
+
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	int *p;
-	unsigned int i;
+	int i;
+	int max;
 
-	if (nmemb == 0 || size == 0)
+	if (nmemb < 0 || size < 0)
 		return (NULL);
+	max = size * nmemb;
 	p = malloc(size * nmemb);
 	if (p == NULL)
 		return (NULL);
-	for (i = 0; i < nmemb ; i++)
+	for (i = 0; i < max ; i++)
 		p[i] = 0;
 	return (p);
 }
