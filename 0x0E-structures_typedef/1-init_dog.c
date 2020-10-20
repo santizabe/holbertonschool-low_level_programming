b8d9c43620fa97300b4fd6c1a2a31dedbac341ca
santizabe <santizabe@gmail.com>
2020-10-20 02:04:37 +0000

    Don't know if it's gonna work

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+* init_dog - initializes dog struct
+* @d: The other structure
+* @name: Dog's name
+* @age: Dawg's age
+* @owner: Puppy's owner
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog *my_dog;
+
+	my_dog->name = *name;
+	my_dog->age = age;
+	my_dog->owner = *owner;
+	return (my_dog);
+}
