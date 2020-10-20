1a701103f1058c762dcdccb0cec157303f572f62
santizabe <santizabe@gmail.com>
2020-10-20 01:54:31 +0000

    Woof

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - structure for Holberton project
+* @name: Well..
+* @age: Ight..
+* @owner: Don't give a f..
+* Pd: F*ck you Django
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+} dog;
+
+#endif
