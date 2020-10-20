5eb7f3bca8360c70be45278403f9ec9e63b7c2b7
santizabe <santizabe@gmail.com>
2020-10-20 02:07:55 +0000

    Here's your puppy

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -12,8 +12,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	struct dog *my_dog;
 
-	my_dog->name = *name;
+	my_dog = d;
+	my_dog->name = name;
 	my_dog->age = age;
-	my_dog->owner = *owner;
-	return (my_dog);
+	my_dog->owner = owner;
 }
