a77f0224535603a2a6eb041efa49d952e8e21387
santizabe <santizabe@gmail.com>
2020-10-20 04:52:59 +0000

    Dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,12 +9,12 @@
 * @owner: Don't give a f..
 * Pd: F*ck you Django
 */
-struct dog
+typedef struct dog
 {
 char *name;
 float age;
 char *owner;
-} dog;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
