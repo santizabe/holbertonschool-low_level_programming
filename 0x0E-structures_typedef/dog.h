9d0a293cf52b47c723cc031fe1efe004a62d5945
santizabe <santizabe@gmail.com>
2020-10-20 04:24:47 +0000

    Puppy got me nuts

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
-
+#include <stdlib.h>
 /**
 * struct dog - structure for Holberton project
 * @name: Well..
@@ -16,4 +16,6 @@ char *owner;
 } dog;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
 #endif
