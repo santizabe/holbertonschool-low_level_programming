e33dbb9aeac5a466d22886aa400d224878b51176
santizabe <santizabe@gmail.com>
2020-10-20 01:14:38 +0000

    Print this

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+* 2-main - Prints the name of the program that is compiled
+* Return - 0 if success
+*/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return(0);
+}
