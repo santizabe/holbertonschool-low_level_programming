ee86f0f0981f3001d6427c7b6b344452635bdb6f
santizabe <santizabe@gmail.com>
2020-10-20 05:35:15 +0000

    Woof

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,9 @@
 #ifndef DOG_H
 #define DOG_H
+
 #include <stdlib.h>
 #include <stdio.h>
+
 /**
 * struct dog - structure for Holberton project
 * @name: Well..
@@ -19,5 +21,6 @@ char *owner;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
