5956382739b28eb480adeb2ae2aa82ab3604b4b7
santizabe <santizabe@gmail.com>
2020-10-22 01:50:27 +0000

    Let's see if it works

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - Iterates over an array
+ * @array: Array to be iterated
+ * @size: size of the array
+ * @action: function to be callbacked
+ * Return: Nothingx
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int a;
+	unsigned int i;
+
+	if (action)
+		{
+			for (i = 0; i < size; i++)
+				{
+					a = array[i];
+					action(a);
+				}
+		}
+	else
+		return;
+}
