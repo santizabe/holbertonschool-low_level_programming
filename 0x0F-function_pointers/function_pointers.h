62cfa25f7db8289fc4b5e75342c5d50841ff2417
santizabe <santizabe@gmail.com>
2020-10-22 01:51:00 +0000

    Done

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,9 @@
 #ifndef FPOINTERS_H
 #define FPOINTERS_H
+#include <stdlib.h>
+#include <stdio.h>
 
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif
