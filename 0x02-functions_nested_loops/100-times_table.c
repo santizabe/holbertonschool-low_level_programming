73d3da2b3efd7214b41e8e14e0684b2bb878ef6e
santizabe <santizabe@gmail.com>
2020-11-20 23:29:12 +0000

    Print table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,45 +7,41 @@
 
 void print_times_table(int n)
 {
-	int a = 0, b = 0, c;
+	int a = 0, b = 0;
 
-	if (n > 15 || n < 0)
+	if (n < 0 || n > 16)
 		return;
-	while (b < (n + 1))
+	for (; b < (n + 1); a++)
 	{
 		if ((b * a) < 10)
 			_putchar((b * a) + '0');
 		else
 		{
-			c = b * a;
-			if (c > 99)
+			if ((a * b) > 99)
 			{
-				_putchar((c / 100) + '0');
-				_putchar(((c / 10) % 10) + '0');
-				_putchar((c % 10) + '0');
+				_putchar(((a * b) / 100) + '0');
+				_putchar((((a * b) / 10) % 10) + '0');
+				_putchar(((a * b) % 10) + '0');
 			}
 			else
 			{
-				_putchar((c / 10) + '0');
-				_putchar((c % 10) + '0');
+				_putchar(((a * b) / 10) + '0');
+				_putchar(((a * b) % 10) + '0');
 			}
 		}
-		a++;
-		if (a <= n)
+		if (a < n)
 		{
 			_putchar(',');
 			_putchar(' ');
-			if ((a * b) < 100)
-			{
-				_putchar(' ');
-				if ((a * b) < 10)
-					_putchar(' ');
-			}
 		}
-		if (a == (n + 1))
+		if ((a * b) < 100)
+			_putchar(' ');
+		if ((a * b) < 10)
+			_putchar(' ');
+		if (a == n)
 		{
 			b++;
-			a = 0;
+			a = -1;
 			_putchar('\n');
 		}
 	}
