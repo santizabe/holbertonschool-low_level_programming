f6b6bef4cf1b084a8a8da75aaaa17bbc38ac66f1
santizabe <santizabe@gmail.com>
2020-11-21 01:15:03 +0000

    series

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+* main - xD
+* Return: 0
+**/
+
+int main(void)
+{
+	double a = 0, b = 1, temp1, temp2;
+	int c;
+
+	for (c = 1; c < 51; c++)
+	{
+		temp1 = a;
+		temp2 = b;
+		a = temp2;
+		b = temp1 + a;
+		if (c < 50)
+			printf("%0.lf, ", b);
+	}
+	printf("%0.lf\n", b);
+
+	return (0);
+}
