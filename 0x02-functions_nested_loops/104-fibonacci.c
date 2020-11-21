69f76ca452b32ca438e128c5d088dce64cebb532
santizabe <santizabe@gmail.com>
2020-11-21 05:41:50 +0000

    Hello fibo

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,19 +7,19 @@
 
 int main(void)
 {
-	unsigned int a = 0, b = 1, temp1, temp2;
+	long double a = 0, b = 1, temp1, temp2;
 	int c;
 
-	for (c = 0; c < 99; c++)
+	for (c = 0; c < 98; c++)
 	{
 		temp1 = a;
 		temp2 = b;
 		a = temp2;
 		b = temp1 + a;
-		if (c < 99)
-                printf("%u, ", b);
+		if (c < 98)
+                printf("%.0Lf, ", b);
 	}
-	printf("%u\n", b);
+	printf("%.0Lf\n", b);
 
 	return (0);
 }
