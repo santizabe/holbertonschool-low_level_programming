ce4a192a65c429b1d2a939a69dbafcd2239363c3
santizabe <santizabe@gmail.com>
2020-11-21 00:27:05 +0000

    Naturals

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+*
+*
+*
+**/
+
+int main(void)
+{
+	int n, sum = 0;
+
+	for (n = 3; n < 1024; n++)
+	{
+		if ((n % 3) == 0 || (n % 5) == 0)
+		{
+			sum += n;
+		}
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
