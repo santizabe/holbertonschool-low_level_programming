4d32545e4bd553400aba65d0d4790fa6c3e12a9a
santizabe <santizabe@gmail.com>
2020-11-29 04:03:11 +0000

    Done

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * _atoi -  convert a string to an integer.
+ * @s: string to convert
+ *
+ * Return: the first number in the string
+ */
+int _atoi(char *s)
+{
+	unsigned int res = 0;
+	int resNeg = 0;
+	int signo = 1;
+
+	while (*s)
+	{
+		if (*s == '-')
+			signo *= -1;
+		if (*s >= '0' && *s <= '9')
+		{
+			res = res * 10 + *s - '0';
+			if (s[1] < '0' || s[1] > '9')
+				break;
+		}
+
+		s++;
+	}
+
+	if (signo < 0)
+		return (resNeg = res * signo);
+	else
+		return (res * signo);
+}
