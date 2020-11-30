4b03a439ffa102ba36dd9b914c1c824165f68410
santizabe <santizabe@gmail.com>
2020-11-30 13:50:59 +0000

    done

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -1,80 +1,39 @@
 #include "holberton.h"
 
 /**
- * print_number - print number
- * @n: number to print
- *
- * Description: calculates lenght and sign
- * sends number to print
- * Return: nothing
- */
+* print_number - F
+* @n: ok
+*
+*/
+
 void print_number(int n)
 {
-	/*(s)signo, (r)esultado, (l)argo*/
-	char s = '+';
-	int l = 0;
-	unsigned int r;
+	int a = 1, i, c, temp, mod;
 
-	/*Guardar signo*/
-	if (n < 0)
+	if (n <= 0)
 	{
-		s = '-';
-		n *= -1;
-		r = n;
-	}
-	r = n;
-
-	/*largo del número*/
-	while (r > 9)
-	{
-		r /= 10;
-		l++;
-	}
-
-	/*imprimir signo*/
-	if (s == '-')
-		_putchar('-');
-
-	/*imprimir número*/
-	print(l, n);
-}
-
-/**
- * print - print number
- * @l: lenght of the number
- * @n: number to print
- *
- * Description: makes calculations and print
- * number from back to the beggining
- * Return: nothing
- */
-void print(int l, int n)
-{
-	int i = 0, j, k = 1, maxL = l;
-	unsigned int r;
-
-	while (i < l)
-	{
-		j = 0;
-		r = n;
-		if (l == maxL)
-		{
-			while (r > 9)
-				r /= 10;
-			_putchar(r + 48);
-		}
+		if (n == 0)
+			_putchar('0');
 		else
-		{
-			while (j < maxL - k)
-			{
-				r /= 10;
-				j++;
-			}
-			_putchar(r % 10 + 48);
-			k++;
-		}
-		l--;
+			_putchar('-');
 	}
-	r = n;
-	_putchar(r % 10 + 48);
+	c = n;
+	for (i = 0; c != 0; i++)
+		c /= 10;
+	temp = i;
+	while (a <= temp)
+	{
+		i = temp;
+		c = n;
+		while ((i - a) > 0)
+		{
+			c /= 10;
+			i--;
+		}
+		mod = c % 10;
+		if (mod < 0)
+			mod *= -1;
+		_putchar(mod + '0');
+		a++;
+	}
 }
