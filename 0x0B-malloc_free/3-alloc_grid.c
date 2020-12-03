f938cabbc0589f3977afb66c8025833007996f5c
santizabe <santizabe@gmail.com>
2020-12-03 19:08:58 +0000

    done

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+
+/**
+*
+*
+*
+*
+*/
+
+int **alloc_grid(int width, int height)
+{
+	int **hello, i, j;
+
+	if (width < 1 || height < 1)
+		return (NULL);
+	hello = malloc(height * sizeof(int));
+	if (hello == NULL)
+	{
+		free(hello);
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		hello[i] = malloc(width * sizeof(int));
+		if (hello[i] == NULL)
+		{
+			free(hello[i]);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+		{
+			hello[i][j] = 0;
+		}
+	}
+
+	return (hello);
+}
