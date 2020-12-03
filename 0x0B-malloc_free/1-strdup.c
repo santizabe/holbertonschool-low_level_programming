a9015b567f8278c2ed925b16487766a286610886
santizabe <santizabe@gmail.com>
2020-12-03 16:34:34 +0000

    Done

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+* _strdup - dups a str
+* @str: src
+* Return: newstr if good, NULL else
+*
+*/
+
+char *_strdup(char *str)
+{
+	int i;
+	char *new;
+
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; str[i]; i++)
+		;
+	new = malloc(temp * sizeof(char));
+	if (new == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	for (i = 0; str[i]; i++);
+	{
+		new[i] = str[i];
+	}
+	new[i] = '\0';
+}
